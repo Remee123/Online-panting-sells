8577c9abba88078790f2988411af437fae4b183b
Remee123 <rajricha1993@gmail.com>
2018-07-15 18:40:11 +0530

    My very first commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+    
+table, th, td
+    {
+        border: 5px solid blue;
+    }
+    #Sign 
+    {
+        margin:20px;
+       
+    
+    
+    }
+    #Sign a
+    {
+        margin: 20px;
+        
+    }
+</style>
+</head>
+    <font size="4"> </font>
+    <h1><center><u>SHADES OF COLOUR</u></center></h1>
+<body background="Sweet-Abstract-Backgrounds-Tumblr-Wallpaper-1024x576.jpg">
+    <div id="Sign">
+<center>
+<a href="my.html">GALLEARY</a>
+<a href="about.html">ABOUT US</a>
+<a href="contact.html">CONTACT US</a>
+<a href="signup.php">SIGN UP</a>
+</center>
+    </div>
+<table align="center">
+
+<tr><th><img src="himanshu3.jpg" alt="sktech" width="250" height="142"></th>
+<th><img src="himanshu8.jpg" alt="sketch" width="250" height="142"></th>
+<th><img src="himanshu12.jpg" alt="sketch" width="250" height="142"></th>
+<th><img src="himanshu7.jpg" alt="sketch" width="250" height="142"></th>
+</tr>
+</table>
+    <br>
+ <table align="center">
+<tr>
+<th><img src="himanshu10.jpg" alt="sktech" width="160" height="150"></th>
+<th><img src="himanshu2.jpg" alt="sketch" width="160" height="150"></th>
+<th><img src="himanshu14.jpg" alt="sketch" width="160" height="150"></th>
+<th><img src="himanshu13.jpg" alt="sketch" width="160" height="150"><a href="my.html">see more..</a></th>
+ </tr>
+    </table>
+    <form >
+<table align="center">
+        <tr>
+         <td>Email :</td> <td><input type="text" name="email" required></td><br>
+        </tr>
+        <tr>
+         <td>Password :</td> <td><input type="password" name="pwd" required></td><br>
+        </tr>
+        <tr>
+         <td><a href="my.html">SIGN IN</a></td>
+           
+<td><a href="sketch.html">cancel</a></td>
+        </tr>
+       </table>
+                                  
+</form>
+     </body>
+     </html>
